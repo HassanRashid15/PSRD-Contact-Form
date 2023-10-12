770730e9c12d88eef3dd59b9bee2a02a66fdb0cb
HassanRashid15 <135802421+HassanRashid15@users.noreply.github.com>
2023-10-12 18:57:53 +0500

    Add files via upload
    Contact Form of PSRD SAMPLE BASED

---
+++ Contact Us11.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function sample() {
+    var m1 = {
+    center: new google.maps.LatLng(52.3, -0.14),
+    zoom: 13,
+    mapss: google.maps.MapTypeId.HYBRID
+    }
+    var m2 = new google.maps.Map(document.getElementById("m"), m1);
+    
+    }
